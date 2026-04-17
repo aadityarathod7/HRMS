887aca1f028c34c30276868706a8fff568cd1152
sjat_paypal <sjat_paypal@paypal.com>
2026-04-17 17:18:29 +0530

    pp

--- nodejs-backend/server.js
+++ nodejs-backend/server.js
@@ -20,8 +20,8 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   credentials: true
 }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 // Routes
 app.use('/auth', require('./routes/auth'));
